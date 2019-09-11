907574240d8b5e2cd9846ff2da78850253f7b114
rvauradkar <rvauradkar@gmail.com>
2019-09-10 20:23:49 -0500

    Changes to breaker from throttle

--- concurrent/ch9/breaker/main.go
+++ concurrent/ch9/breaker/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	//"errors"
+	"fmt"
 	"time"
 
 	"github.com/pkg/errors"
 )
 
-func main() {
-	errors.New("")
+func command() {
+	time.Sleep(10 * time.Millisecond)
+	fmt.Println("Executing command.....")
 }
 
+func main() {
+	errors.New("")
+	fmt.Println("Throttle demo....")
+	breaker := &breaker{}
+	breaker.init("name", 10*time.Millisecond, 3)
+	for i := 0; i < 5; i++ {
+		go func(i int) {
+			err := breaker.execute(command)
+			if c := <-err; c != nil {
+				fmt.Println("Err = ", i, "___", c, "___")
+			}
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
+
+type commandFunc func()
+
+// Handler is a means for client of circuit breaker to provide a method value bound to a struct that will make actual service call
+type Handler func() error
+
 // Breaker struct for circuit breaker control parameters
-type Breaker struct {
+type breaker struct {
 	name          string
 	timeout       time.Duration
 	numConcurrent int
@@ -20,15 +42,26 @@ type Breaker struct {
 }
 
 // New initializes the circuit breaker
-func (b *Breaker) New(name string, timeout time.Duration, numConcurrent int) {
+func (b *breaker) init(name string, timeout time.Duration, numConcurrent int) {
 	b.name = name
 	b.timeout = timeout
 	b.numConcurrent = numConcurrent
 	b.semaphore = make(chan bool, b.numConcurrent)
 }
-
-// Handler is a means for client of circuit breaker to provide a method value bound to a struct that will make actual service call
-type Handler func() error
+func (b *breaker) execute(command commandFunc) chan error {
+	errorch := make(chan error, 1)
+	go func() {
+		select {
+		case b.semaphore <- true:
+			defer func() { <-b.semaphore }()
+			command()
+			errorch <- nil
+		default:
+			errorch <- errors.New("reached threshold, cannot run your function")
+		}
+	}()
+	return errorch
+}
 
 type DNSConfigError struct {
 	Err error
@@ -38,5 +71,3 @@ func (e *DNSConfigError) Unwrap() error   { return e.Err }
 func (e *DNSConfigError) Error() string   { return "error reading DNS config: " + e.Err.Error() }
 func (e *DNSConfigError) Timeout() bool   { return false }
 func (e *DNSConfigError) Temporary() bool { return false }
-
-//func (b *Breaker) Execute()
