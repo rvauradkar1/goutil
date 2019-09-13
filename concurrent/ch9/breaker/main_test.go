1a1054a8bce44685c59156bf7a859048ae6836f5
rvauradkar1 <rvauradkar1@gmail.com>
2019-09-12 19:53:18 -0500

    Update main_test.go

--- concurrent/ch9/breaker/main_test.go
+++ concurrent/ch9/breaker/main_test.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 	"testing"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 func Test_is_ok(t *testing.T) {
@@ -27,12 +31,12 @@ func Test_shutdown(t *testing.T) {
 	}
 }
 
-func Test_scanner_circuit_still_bad(t *testing.T) {
+func Test_scanner_circuit_repaired(t *testing.T) {
 	b := &breaker{}
 	b.init("name", time.Second, 0)
 	b.isOk = false
 	b.healthCheckInterval = 10
-	fmt.Println("starting Test_scanner_circuit_still_bad")
+	fmt.Println("starting Test_scanner_circuit_repaired")
 	time.Sleep(15 * time.Millisecond)
 	fmt.Println("Return = ", b.status)
 	if b.status != iCircuitStillBad {
@@ -42,6 +46,79 @@ func Test_scanner_circuit_still_bad(t *testing.T) {
 	fmt.Println("Return = ", b.status)
 }
 
+func Test_execute_t(t *testing.T) {
+	errors.New("")
+	fmt.Println("Throttle demo....")
+	commands := &wrapper{}
+	b := &breaker{}
+	b.init("name", 10*time.Millisecond, 3)
+	b.healthCheckInterval = 1000
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func(i int) {
+			defer wg.Done()
+			err := b.execute(commands)
+			if c := <-err; c != nil {
+				fmt.Println("Err = ", i, "___", c, "___")
+			}
+		}(i)
+	}
+	fmt.Println("Number of go routines = ", runtime.NumGoroutine())
+	wg.Wait()
+	fmt.Printf("isOk 1 = %v\n", b.isOk)
+	time.Sleep(200 * time.Millisecond)
+	fmt.Printf("isOk 2 = %v\n", b.isOk)
+	b.shutdown()
+	fmt.Println("Done!!")
+}
+
+func Test_execute_1(t *testing.T) {
+	errors.New("")
+	fmt.Println("Throttle demo....")
+	b := &breaker{}
+	b.init("name", 10*time.Millisecond, 3)
+	if !b.isOk {
+		t.Errorf("Circuit should be ok")
+	}
+	if b.isShutdown {
+		t.Errorf("Circuit should NOT be shutdown")
+	}
+	b.shutdown()
+}
+
+func Test_execute_2(t *testing.T) {
+	errors.New("")
+	fmt.Println("Running Test_execute_2 demo....")
+	b := &breaker{}
+	b.init("name", 10*time.Millisecond, 3)
+	w := &wrapper{exec: false}
+	b.execute(w)
+	time.Sleep(5 * time.Millisecond)
+	if !w.exec {
+		t.Errorf("Service should have executed")
+	}
+	b.shutdown()
+}
+
+func Test_execute_exceed_limit_wait_till_circuit_ok(t *testing.T) {
+	errors.New("")
+	fmt.Println("Running Test_execute_exceed_limit_wait_till_circuit_ok demo....")
+	b := &breaker{}
+	b.init("name", 10*time.Millisecond, 3)
+	b.healthCheckInterval = 1000
+	w := &wrapper2{exec: false}
+	b.execute(w)
+	w = &wrapper2{exec: false}
+	b.execute(w)
+	w = &wrapper2{exec: false}
+	b.execute(w)
+	time.Sleep(999 * time.Millisecond)
+	if !w.exec {
+		t.Errorf("Service should have executed")
+	}
+	b.shutdown()
+}
 func Test_scanner_circuit_reset(t *testing.T) {
 	b := &breaker{}
 	b.init("name", time.Second, 1)
