6d85dd4fe1330e950d6b9a16c5f710bbc7fd685a
rvauradkar <rvauradkar@gmail.com>
2019-09-21 11:39:08 -0500

    Started adding http samples

--- concurrent/ch7/advancedcompose/main.go
+++ concurrent/ch7/advancedcompose/main.go
@@ -7,37 +7,35 @@ import (
 )
 
 func main() {
-	ch, _ := service1("Start...")
-	println("called service1 ", ch)
-	ch1, _, _ := service2(ch)
-	println("called service2 ", ch1)
-	for s := range ch1 {
-		println(s)
+	ch, _ := task1()
+	println("called task1 ", ch)
+	for s := range ch {
+		fmt.Println(s.s, "---", s.err)
 	}
 }
 
-type s1Resp struct {
+type resp struct {
 	s   string
 	err error
 }
 
-func service1(s string) (<-chan s1Resp, error) {
-	out := make(chan s1Resp)
+func task1() (<-chan resp, error) {
+	out := make(chan resp)
 	go func() {
 		defer close(out)
 		for i := 0; i < 5; i++ {
 			if i == 3 {
-				s1 := s1Resp{err: errors.Errorf("Service failure on %d", i)}
+				s1 := resp{err: errors.Errorf("Service failure on %d", i)}
 				out <- s1
-				return
+			} else {
+				out <- resp{s: fmt.Sprintf("Performing Task %d", i)}
 			}
-			out <- s1Resp{s: fmt.Sprintf(s+"\n"+"Performing Service %d\n", i)}
 		}
 	}()
 	return out, nil
 }
 
-func service2(in <-chan s1Resp) (<-chan string, chan<- error, error) {
+func task2(in <-chan resp) (<-chan string, chan<- error, error) {
 	out := make(chan string)
 	go func() {
 		defer close(out)
